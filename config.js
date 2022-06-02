be4ed07cc107b7dc5bd8cdd477243c3cebf575d6
VuvanToan2102 <106741484+VuvanToan2102@users.noreply.github.com>
2022-06-02 17:41:52 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
+    titleWeb: "love",
+    introTitle: 'Cái này mình tạo để gửi cho cậu đó Thanh',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
