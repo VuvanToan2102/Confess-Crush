3e0705c2fd72683cb1f9d97bcc528a1984a72792
VuvanToan2102 <106741484+VuvanToan2102@users.noreply.github.com>
2022-06-02 18:14:31 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,20 @@
 const CONFIG = {
     titleWeb: "Love",
-    introTitle: 'Cái này mình làm để gửi đến Thanh',
-    introDesc: `Trái đất vốn lạ 
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
+    introTitle: 'Cái này mình làm để gửi đến bạn tên Thanh',
+    introDesc: `Cậu là một cô gái xinh đẹp ...
+    Và còn rất giỏi nữa ,,,
+    Tớ thích cậu thì chắc điều đó cậu cũng biết ^ ^
+    Tớ không biết thích cậu từ baoh nhưng tớ nghĩ là lần đầu tiên`,
     btnIntro: '^^HiHi^^',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
+    title: 'Phải chăng tớ đã thích ngay từ cái nhìn đầu tiên 🥰',
+    desc: 'Phải chăng tớ đã say ngay từ lúc thấy nụ cười ấy ',
     btnYes: 'Thích lắm <33',
     btnNo: 'Không nha :3',
     question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
     btnReply: 'Gửi cho bạn <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
     mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
+    messDesc: 'Nào dẫn tớ đi uống hoa quả ngâm nha ^ ^.',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
 }
